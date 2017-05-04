5a766b0beb58dca48892eebffcddc230cf2e62d2
wenbegn <wenbegn@wenbegndeMacBook-Pro.local>
2017-05-04 14:55:39 +0800

    Fewefe

---
+++ ChatKit-OC/Example/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  LCChatKitExample.m
+//  LeanCloudChatKit-iOS
+//
+//  v0.7.19 Created by ElonChan (微信向我报BUG:chenyilong1010) on 16/2/24.
+//  Copyright © 2016年 LeanCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
